7887e1b24fd7ce18fe39f486bbc039bc92fef7de
Fouad <157938552+FouadCoder@users.noreply.github.com>
2026-02-02 04:37:51 +0200

    feat(notifications): add notification screen

--- lib/core/extensions/datetime_style.dart
+++ lib/core/extensions/datetime_style.dart
@@ -12,4 +12,25 @@ extension DateTimeExtension on DateTime {
   String toMonthName() {
     return DateFormat('MMMM').format(this); // e.g. "September"
   }
+
+  String toSmartTimeAgo() {
+    final now = DateTime.now();
+    final diff = now.difference(this);
+
+    if (diff.inMinutes < 2) {
+      return 'time_just_now'.tr();
+    } else if (diff.inMinutes < 60) {
+      return "${diff.inMinutes} ${'time_minutes_ago'.tr()}";
+    } else if (diff.inHours < 24 && now.day == day) {
+      return 'time_today'.tr();
+    } else if (diff.inHours < 48 && now.day - day == 1) {
+      return 'time_yesterday'.tr();
+    } else if (diff.inDays < 7) {
+      return "${diff.inDays} ${'time_days_ago'.tr()}";
+    } else if (diff.inDays < 14) {
+      return 'time_week_ago'.tr();
+    } else {
+      return DateFormat('MMM d').format(this); // e.g. "Sep 15"
+    }
+  }
 }
